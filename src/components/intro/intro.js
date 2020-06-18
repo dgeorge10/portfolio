83ccb2f8327f2bb50a3c49712804a928eaa97f33
Dennis <dennisgeorge10@gmail.com>
2020-06-17 21:05:27 -0400

    making intro look nicer on mobile

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -7,18 +7,18 @@ export default class Introduction extends React.Component {
 	render () {
 	  return (
 			<div id="intro">
-				<Container>
+				<Container fluid>
 				  <Row>
-					<Col>
+					<Col lg="6">
 						<div id="profile-pic-container">
 						   <img id="profile-pic" src="img/me.jpg" alt="personality pic"/> 	
 						</div>
 					</Col>
-					<Col>
+					<Col lg="6">
 						<div id="i-am-a-container">
 							<h1 id="i-am-a"><strong>I am a developer.</strong></h1>
 							<br/>
-							<h3 id="mission">lil baby statement.</h3>
+							<h6 id="mission">Committed to collaborative problem solving, elegant design, and creating a quality product.</h6>
 						</div>
 					</Col>
 				  </Row>
--- src/components/loading/loading.js
+++ src/components/loading/loading.js
@@ -7,7 +7,6 @@ export default class Loading extends React.Component{
    render(){
 	  return (
 		 <div id="loadingChild">
-
 		 	<p>Hi, I'm Dennis :)</p>
 		 	<center>
 		 		<BounceLoader color={"#B15353"} />
